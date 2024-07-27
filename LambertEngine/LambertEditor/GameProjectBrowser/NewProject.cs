13390683e7a3d17f1dd1f6506dbfcb7debb80d00
nael2965 <nael2965@gmail.com>
2024-07-27 21:31:52 +0900

    Sub tool added

---
+++ LambertEngine/LambertEditor/GameProjectBrowser/NewProject.cs
@@ -0,0 +1,114 @@
+﻿using LambertEditor.Common;
+using LambertEditor.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace LambertEditor.GameProjectBrowser
+{
+    [DataContract]
+    public class ProjectTemplate
+    {
+        ~ProjectTemplate() { Console.WriteLine("Delete tamplate"); }
+        [DataMember]
+        public string ProjectType { get; set; }
+        [DataMember]
+        public string ProjectFile { get; set; }
+        [DataMember]
+        public List<string> Folders { get; set; }
+
+        public byte[] Icon { get; set; }
+        public byte[] Screenshot { get; set; }
+        public string IconFilePath { get; set; }
+        public string ScreenshotFilePath { get; set; }
+        public string ProjectFilePath { get; set; }
+
+
+
+    }
+
+    class NewProject : ViewModelBase
+    {
+
+        //TODO: Get Engine installation path
+        private readonly string _templatePath = @"..\..\LambertEditor\ProjectTemplates\";
+
+        private string _projectName = "NewProject";
+        public string ProjectName
+        {
+            get => _projectName;
+            set
+            {
+                if (_projectName != value)
+                {
+                    _projectName = value;
+                    OnPropertyChanged(nameof(ProjectName));
+                }
+            }
+        }
+
+        private string _projectPath = $@"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\LambertProject\";
+        public string ProjectPath
+        {
+            get => _projectPath;
+            set
+            {
+                if (_projectPath != value)
+                {
+                    _projectPath = value;
+                    OnPropertyChanged(nameof(ProjectPath));
+                }
+            }
+        }
+
+
+        private ObservableCollection<ProjectTemplate> _projectTemplate = new ObservableCollection<ProjectTemplate>();
+        public ReadOnlyObservableCollection<ProjectTemplate> ProjectTemplates
+        { get; }
+
+        public NewProject()
+        {
+            ProjectTemplates = new ReadOnlyObservableCollection<ProjectTemplate>(_projectTemplate);
+            try
+            {
+                var templatesFiles = Directory.GetFiles(_templatePath, "template.xml", SearchOption.AllDirectories);
+                Debug.Assert(templatesFiles.Any());
+                foreach (var file in templatesFiles)
+                {
+                    var template = Serializer.FromFile<ProjectTemplate>(file);
+                    template.IconFilePath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(file), "Icon.png "));
+                    template.Icon = File.ReadAllBytes(template.IconFilePath);
+                    template.ScreenshotFilePath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(file), "Screenshot.png "));
+                    template.Screenshot = File.ReadAllBytes(template.ScreenshotFilePath);
+                    template.ProjectFilePath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(file), template.ProjectFile));
+
+                    _projectTemplate.Add(template);
+                }   
+            }
+            catch(Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                //TODO: Log Error
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+}
---
+++ LambertEngine/LambertEditor/Utilities/Serializer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambertEditor.Utilities
+{
+    public static class Serializer
+    {
+        public static void ToFile<T>(T instances, string path){
+            try
+            {
+                using var fs = new FileStream(path, FileMode.Create);
+                var serializer = new DataContractSerializer(typeof(T));
+                serializer.WriteObject(fs, instances);
+            }
+            catch (Exception ex)
+            {
+                Debug.Write(ex.Message);
+                //TODO: error log 
+
+            }
+        }
+        
+        public static T FromFile<T>(string path){
+            try{
+                using var fs = new FileStream(path, FileMode.Open);
+                var serializer = new DataContractSerializer(typeof(T));
+                T instance = (T)serializer.ReadObject(fs);
+                Debug.Write("a");
+                return instance;
+            }catch (Exception ex){
+                Debug.Write(ex.Message);
+                Debug.Write("B");
+                //TODO: error log
+                return default(T);
+            }
+        }
+
+
+
+
+
+    }
+}
