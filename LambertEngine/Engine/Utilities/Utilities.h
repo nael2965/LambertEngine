cdb120cf17b78d77784c75252d23e6dfa64c765e
nael2965 <nael2965@gmail.com>
2024-07-31 23:57:52 +0900

    add utilities

---
+++ LambertEngine/Engine/Utilities/Utilities.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#define USE_STL_VECTOR 1
+#define USE_STL_DEQUE 1
+
+#if USE_STL_VECTOR
+#include <vector>
+namespace lambert::utl
+{
+    template<typename T>
+    using vector = std::vector<T>;
+}
+#endif
+
+#if USE_STL_DEQUE
+#include <deque>
+namespace lambert::utl
+{
+    template<typename T>
+    using deque = std::deque<T>;
+}
+#endif
+
+namespace lambert::utl
+{
+    // TODO: implement own containers
+}
