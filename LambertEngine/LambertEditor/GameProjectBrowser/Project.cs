0b1ead465533243612b026a06d8f7853b405c84b
nael2965 <nael2965@gmail.com>
2024-08-06 21:32:10 +0900

    style: cleanup

--- LambertEngine/LambertEditor/GameProjectBrowser/Project.cs
+++ LambertEngine/LambertEditor/GameProjectBrowser/Project.cs
@@ -1,70 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.IO;
 using System.Runtime.Serialization;
-using System.Text;
 using System.Windows;
 using LambertEditor.Common;
 using LambertEditor.Utilities;
 
+namespace LambertEditor.GameProjectBrowser;
 
-namespace LambertEditor.GameProjectBrowser
+[DataContract(Name = "Game")]
+public class Project : ViewModelBase
 {
-    [DataContract(Name = "Game")]
-    public class Project : ViewModelBase
+    public static string Extension { get; } = ".lambert";
+    [DataMember] public string Name { get; private set; } = "New Project";
+    [DataMember] public string Path { get; private set; }
+    public string FullPath => $"{Path}{Name}{Extension}";
+
+    [DataMember(Name = "Scenes")] private ObservableCollection<Scene> _scenes = new();
+    public ReadOnlyObservableCollection<Scene> Scenes { get; private set; }
+
+    private Scene _activeScene;
+
+    public Scene ActiveScene
     {
-        public static string Extension { get; } = ".lambert";
-        [DataMember] public string Name { get; private set; } = "New Project";
-        [DataMember] public string Path { get; private set; }
-        public string FullPath => $"{Path}{Name}{Extension}";
-        
-        [DataMember(Name = "Scenes")] private ObservableCollection<Scene> _scenes = new ObservableCollection<Scene>();
-        public ReadOnlyObservableCollection<Scene> Scenes { get; private set; }
-
-        private Scene _activeScene;
-        public Scene ActiveScene
-        { 
-            get => _activeScene;
-            set
+        get => _activeScene;
+        set
+        {
+            if (_activeScene != value)
             {
-                if (_activeScene != value)
-                {
-                    _activeScene = value;
-                    OnPropertyChanged(nameof(ActiveScene));
-                }
+                _activeScene = value;
+                OnPropertyChanged(nameof(ActiveScene));
             }
         }
-        public static Project Current => Application.Current.MainWindow.DataContext as Project;
+    }
 
-        public static Project Load(string file)
-        {
-            Debug.Assert(File.Exists(file));
-            return Serializer.FromFile<Project>(file);
-        }
-        public void Unload(){}
+    public static Project Current => Application.Current.MainWindow.DataContext as Project;
 
-        public static void Save(Project project)
-        {
-            Serializer.ToFile(project, project.FullPath);
-        }
+    public static Project Load(string file)
+    {
+        Debug.Assert(File.Exists(file));
+        return Serializer.FromFile<Project>(file);
+    }
 
-        [OnDeserialized] private void OnDeserialized(StreamingContext context)
-        {
-            if (_scenes != null)
-            {
-                Scenes = new ReadOnlyObservableCollection<Scene>(_scenes);
-                OnPropertyChanged(nameof(Scenes));
-            }
-        }
+    public void Unload()
+    {
+    }
 
-        public Project(string name, string path)
+    public static void Save(Project project)
+    {
+        Serializer.ToFile(project, project.FullPath);
+    }
+
+    [OnDeserialized]
+    private void OnDeserialized(StreamingContext context)
+    {
+        if (_scenes != null)
         {
-            Name = name;
-            Path = path;
-            
-            OnDeserialized(new StreamingContext());
+            Scenes = new ReadOnlyObservableCollection<Scene>(_scenes);
+            OnPropertyChanged(nameof(Scenes));
         }
-    }    
+    }
+
+    public Project(string name, string path)
+    {
+        Name = name;
+        Path = path;
+
+        OnDeserialized(new StreamingContext());
+    }
 }
