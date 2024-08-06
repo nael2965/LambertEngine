1f55de358aa2bd9fa357f5fac54f98e201278a8a
nael2965 <nael2965@gmail.com>
2024-08-07 06:49:40 +0900

    feat: Project Layout
    display scenes on editor

---
+++ LambertEngine/LambertEditor/Editors/WorldEditor/ProjectLayoutView.xaml.cs
@@ -0,0 +1,13 @@
+﻿    using System.Windows.Controls;
+
+    namespace LambertEditor.Editors
+    {
+        public partial class ProjectLayoutView : UserControl
+        {
+            public ProjectLayoutView()
+            {
+                InitializeComponent();
+            }
+        }
+    }
+
