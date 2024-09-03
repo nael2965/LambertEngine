b4290fd69a9e4c7ec4dc652c0be9682200d13beb
nael2965 <nael2965@gmail.com>
2024-09-03 12:33:44 +0900

    feat: Game Entity

---
+++ LambertEngine/LambertEditor/Components/Component.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+using LambertEditor.Common;
+
+namespace LambertEditor.Components;
+
+public class Component : ViewModelBase
+{
+    public GameEntity Owner { get; private set; }
+
+    public Component(GameEntity owner)
+    {
+        Debug.Assert(owner != null);
+        Owner = owner;
+    }
+}
---
+++ LambertEngine/LambertEditor/Components/GameEntity.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.Serialization;
+using LambertEditor.Common;
+using LambertEditor.GameProjectBrowser;
+
+namespace LambertEditor.Components;
+
+[DataContract]
+public class GameEntity : ViewModelBase
+{
+    private string _name;
+    [DataMember]
+    public string Name
+    {
+        get => _name;
+        set
+        {
+            if (value != _name)
+            {
+                _name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+    }
+    [DataMember]
+    public Scene ParentScene { get; private set; }
+    
+    [DataMember(Name = nameof(Components))]
+    private readonly ObservableCollection<Component> _components = new ObservableCollection<Component>();
+    public ObservableCollection<Component> Components { get; }
+    
+    public GameEntity(Scene scene, ObservableCollection<Component> components)
+    {
+        Debug.Assert(scene != null);
+        ParentScene = scene;
+        Components = components;
+    }
+}
